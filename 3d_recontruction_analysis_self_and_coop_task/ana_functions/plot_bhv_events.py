8eb2ae1fbcbec12a3d8915b13008135a664d6ebd
wilburshi <lajumboshiwk@gmail.com>
2023-04-13 18:28:38 -0400

    change the angle threshold

--- 3d_recontruction_analysis_self_and_coop_task/ana_functions/plot_bhv_events.py
+++ 3d_recontruction_analysis_self_and_coop_task/ana_functions/plot_bhv_events.py
@@ -23,12 +23,12 @@ def plot_bhv_events(date_tgt, animal1, animal2, session_start_time, session_plot
         for itime in oneway_gaze1:
             line2, = axs[0].plot([itime,itime],[0,1],linewidth = 2.0,color=(0.7,0.0,0.7),label = 'one-way gaze')  
     except:
-        print("no oneway gaze")
+        print("no oneway gaze "+animal1)
     try:
         for itime in mutual_gaze1:
             line3, = axs[0].plot([itime,itime],[0,1],linewidth = 2.0,color=(0.7,0.7,0.0),label = 'mutual gaze')  
     except:
-        print("no mutual gaze")
+        print("no mutual gaze "+animal1)
     axs[0].set_title(date_tgt+' '+animal1,fontsize = 18)
     axs[0].set_xlim([-10,session_plot_time+10])
     axs[0].set_xlabel("")
@@ -53,12 +53,12 @@ def plot_bhv_events(date_tgt, animal1, animal2, session_start_time, session_plot
         for itime in oneway_gaze2:
             line2, = axs[1].plot([itime,itime],[0,1],linewidth = 2.0,color=(0.7,0.0,0.7))    
     except:
-        print("no oneway gaze")
+        print("no oneway gaze"+animal2)
     try:
         for itime in mutual_gaze2:
             line3, = axs[1].plot([itime,itime],[0,1],linewidth = 2.0,color=(0.7,0.7,0.0))    
     except:
-        print("no mutual gaze")
+        print("no mutual gaze"+animal2)
     axs[1].set_title(date_tgt+' '+animal2,fontsize = 18)
     axs[1].set_xlim([-10,session_plot_time+10])
     axs[1].set_xlabel("time/s",fontsize = 19)
