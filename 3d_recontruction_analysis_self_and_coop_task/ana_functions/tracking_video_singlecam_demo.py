5b28b4ea388a3ea62cf6d0d8ba0ec2c4f8d854e8
wilburshi <lajumboshiwk@gmail.com>
2023-04-17 22:31:07 -0400

    update the way to make demo videos, make a slurm job file to make it possible to make demo video, instead of using jypyter notebook

--- 3d_recontruction_analysis_self_and_coop_task/ana_functions/tracking_video_singlecam_demo.py
+++ 3d_recontruction_analysis_self_and_coop_task/ana_functions/tracking_video_singlecam_demo.py
@@ -1,9 +1,10 @@
 #  function - make demo videos for the body part tracking based on single camera, also show the important axes
-def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_allvectors,output_allangles,lever_loc_both, tube_loc_both,animal1,animal2,animalnames_videotrack,bodypartnames_videotrack,date_tgt,nframes):
+def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_allvectors,output_allangles,lever_loc_both, tube_loc_both,animalnames_videotrack,bodypartnames_videotrack,date_tgt,animal1_filename,animal2_filename,nframes):
 
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
     import scipy
     import string
     import warnings
@@ -26,7 +27,7 @@ def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_a
     import matplotlib.animation as animation
 
     # Settings
-    video_file = "example_videos_singlecam_demo/"+date_tgt+"_cam2only_tracking_demo.mp4"
+    video_file = "../3d_recontruction_analysis_self_and_coop_task/example_videos_singlecam_demo/"+date_tgt+"_"+animal1_filename+animal2_filename+"_cam2only_tracking_demo.mp4"
     clear_frames = True     # Should it clear the figure between each frame?
     fps = 30
 
@@ -45,24 +46,58 @@ def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_a
     
    
     # set up the figure setting  
-    fig = plt.figure(figsize = (10,5))
-    ax = fig.subplots(1,1)
-    ax.set_xlim([0,1920])
-    ax.set_ylim([0,1080])
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.invert_yaxis()
+    fig = plt.figure(figsize = (15,12))
+    gs=GridSpec(5,3) # 5 rows, 3 columns
+
+    ax1=fig.add_subplot(gs[0:2,:]) # animal tracking frame
+    ax2=fig.add_subplot(gs[3,:]) # animal1 behavioral events
+    ax3=fig.add_subplot(gs[4,:]) # animal2 behavioral events
+
+    ax1.set_xlim([0,1920])
+    ax1.set_ylim([0,1080])
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('y')
+    ax1.invert_yaxis()
+    
+    ax2.set_xlim([0,nframes])  
+    ax2.set_ylim([0,1])
+    ax2.set_xlabel('frame number')
+    ax2.set_ylabel('')
+    ax3.set_xlim([0,nframes])  
+    ax3.set_ylim([0,1])
+    ax3.set_xlabel('frame number')
+    ax3.set_ylabel('')
+
 
     with writer.saving(fig, video_file, 100):
         for iframe in np.arange(0,nframes,1):    
+           
+            print("printing frame ",str(iframe+1),"/",str(nframes))
+        
             if clear_frames:
                 fig.clear()
-                ax = fig.subplots(1,1)
-                ax.set_xlim([0,1920])
-                ax.set_ylim([0,1080])
-                ax.set_xlabel('x')
-                ax.set_ylabel('y')
-                ax.invert_yaxis()
+                gs=GridSpec(5,3) # 5 rows, 3 columns
+
+                ax1=fig.add_subplot(gs[0:2,:]) # animal tracking frame
+                ax2=fig.add_subplot(gs[3,:]) # animal1 behavioral events
+                ax3=fig.add_subplot(gs[4,:]) # animal2 behavioral events
+
+                ax1.set_xlim([0,1920])
+                ax1.set_ylim([0,1080])
+                ax1.set_xlabel('x')
+                ax1.set_ylabel('y')
+                ax1.invert_yaxis()
+                
+                ax2.set_xlim([0,nframes])  
+                ax2.set_ylim([0,1])
+                ax2.set_xlabel('frame number')
+                ax2.set_ylabel('')
+                ax2.set_title('animal 1 behavioral events')
+                ax3.set_xlim([0,nframes])  
+                ax3.set_ylim([0,1])
+                ax3.set_xlabel('frame number')
+                ax3.set_ylabel('')
+                ax3.set_title('animal 2 behavioral events')
 
             
             for ianimal in np.arange(0,nanimals,1):    
@@ -75,7 +110,10 @@ def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_a
                     ibdpart_name = body_parts_unique[ibdpart]
                     bodypart_loc_iframe[ibdpart,:] = np.array(bodyparts_locs_camN[(ianimal_name,ibdpart_name)])[iframe,:]
                 # plot the body parts
-                ax.plot(bodypart_loc_iframe[:,0], bodypart_loc_iframe[:,1], '.', color=colors[ianimal])
+                if (ianimal==0): 
+                    ax1.plot(bodypart_loc_iframe[:,0], bodypart_loc_iframe[:,1], '.', color=colors[ianimal],label ='animal1')
+                else:
+                    ax1.plot(bodypart_loc_iframe[:,0], bodypart_loc_iframe[:,1], '.', color=colors[ianimal],label ='animal2')
                 
                 # draw skeleton                
                 for iskel in np.arange(0,nskeletons,1):
@@ -89,32 +127,86 @@ def tracking_video_singlecam_demo(bodyparts_locs_camN,output_look_ornot,output_a
                         skelbody12_loc_iframe[0,:] = np.array(bodyparts_locs_camN[(ianimal_name,skel_body1_name)])[iframe,:]
                         skelbody12_loc_iframe[1,:] = np.array(bodyparts_locs_camN[(ianimal_name,skel_body2_name)])[iframe,:]
                         # plot one skeleton
-                        ax.plot(skelbody12_loc_iframe[:,0],skelbody12_loc_iframe[:,1],'-',color=colors[ianimal])
+                        ax1.plot(skelbody12_loc_iframe[:,0],skelbody12_loc_iframe[:,1],'-',color=colors[ianimal])
                     except:
                         continue
 
                 
                 # draw lever and tube location
-                ax.plot(lever_loc_both[ianimal_name][0],lever_loc_both[ianimal_name][1],'o',color='g')
-                ax.plot(tube_loc_both[ianimal_name][0],tube_loc_both[ianimal_name][1],'o',color='y')
+                if (ianimal==1): 
+                    ax1.plot(lever_loc_both[ianimal_name][0],lever_loc_both[ianimal_name][1],'o',color='g',label='lever')
+                    ax1.plot(tube_loc_both[ianimal_name][0],tube_loc_both[ianimal_name][1],'o',color='y',label='tube')
+                else:
+                    ax1.plot(lever_loc_both[ianimal_name][0],lever_loc_both[ianimal_name][1],'o',color='g')
+                    ax1.plot(tube_loc_both[ianimal_name][0],tube_loc_both[ianimal_name][1],'o',color='y')
            
                 # draw head vector
                 rightEye_loc_iframe = np.array(bodyparts_locs_camN[(ianimal_name,'rightEye')])[iframe,:]
                 leftEye_loc_iframe = np.array(bodyparts_locs_camN[(ianimal_name,'leftEye')])[iframe,:]
                 meaneye_loc_iframe = np.nanmean(np.vstack([rightEye_loc_iframe,leftEye_loc_iframe]),axis=0)
                 head_loc_iframe = meaneye_loc_iframe + 200*np.array(output_allvectors['head_vect_all_merge'][ianimal_name])[iframe,:]
-                ax.plot([meaneye_loc_iframe[0],head_loc_iframe[0]],[meaneye_loc_iframe[1],head_loc_iframe[1]],'-',color = '0.75')
+                if (ianimal==1):
+                    ax1.plot([meaneye_loc_iframe[0],head_loc_iframe[0]],[meaneye_loc_iframe[1],head_loc_iframe[1]],'-',color = '0.75',label='head axis')
+                else:
+                    ax1.plot([meaneye_loc_iframe[0],head_loc_iframe[0]],[meaneye_loc_iframe[1],head_loc_iframe[1]],'-',color = '0.75')     
 
                 # draw other - eye vector            
                 other_loc_iframe = meaneye_loc_iframe + 200*np.array(output_allvectors['other_eye_vect_all_merge'][ianimal_name])[iframe,:]
-                ax.plot([meaneye_loc_iframe[0],other_loc_iframe[0]],[meaneye_loc_iframe[1],other_loc_iframe[1]],'-',color = colors[np.absolute(ianimal*2-1)])
+                if (ianimal==1):
+                    ax1.plot([meaneye_loc_iframe[0],other_loc_iframe[0]],[meaneye_loc_iframe[1],other_loc_iframe[1]],'-',color = colors[np.absolute(ianimal-1)],label="eye-other")
+                else:
+                    ax1.plot([meaneye_loc_iframe[0],other_loc_iframe[0]],[meaneye_loc_iframe[1],other_loc_iframe[1]],'-',color = colors[np.absolute(ianimal-1)])
 
                 # draw tube - eye vector
                 tube_loc_iframe = meaneye_loc_iframe + 200*np.array(output_allvectors['tube_eye_vect_all_merge'][ianimal_name])[iframe,:]
-                ax.plot([meaneye_loc_iframe[0],tube_loc_iframe[0]],[meaneye_loc_iframe[1],tube_loc_iframe[1]],'-',color = 'y')
+                if (ianimal==1):
+                    ax1.plot([meaneye_loc_iframe[0],tube_loc_iframe[0]],[meaneye_loc_iframe[1],tube_loc_iframe[1]],'-',color = 'y',label="eye-tube")
+                else:
+                    ax1.plot([meaneye_loc_iframe[0],tube_loc_iframe[0]],[meaneye_loc_iframe[1],tube_loc_iframe[1]],'-',color = 'y')
 
                 # draw lever - eye vector
                 lever_loc_iframe = meaneye_loc_iframe + 200*np.array(output_allvectors['lever_eye_vect_all_merge'][ianimal_name])[iframe,:]
-                ax.plot([meaneye_loc_iframe[0],lever_loc_iframe[0]],[meaneye_loc_iframe[1],lever_loc_iframe[1]],'-',color = 'g')
+                if (ianimal==1):
+                    ax1.plot([meaneye_loc_iframe[0],lever_loc_iframe[0]],[meaneye_loc_iframe[1],lever_loc_iframe[1]],'-',color = 'g',label="eye-lever")
+                else:
+                    ax1.plot([meaneye_loc_iframe[0],lever_loc_iframe[0]],[meaneye_loc_iframe[1],lever_loc_iframe[1]],'-',color = 'g')
+
+
+                ax1.legend()
+                         
+
+
+                # draw animal behavioral events
+                look_at_other_framenum_all = np.where(np.array(output_look_ornot["look_at_other_or_not_merge"][ianimal_name])==1)[0]
+                look_at_other_framenum_plot = look_at_other_framenum_all[look_at_other_framenum_all<iframe]
+                look_at_lever_framenum_all = np.where(np.array(output_look_ornot["look_at_lever_or_not_merge"][ianimal_name])==1)[0]
+                look_at_lever_framenum_plot = look_at_lever_framenum_all[look_at_lever_framenum_all<iframe]
+                look_at_tube_framenum_all = np.where(np.array(output_look_ornot["look_at_tube_or_not_merge"][ianimal_name])==1)[0]
+                look_at_tube_framenum_plot = look_at_tube_framenum_all[look_at_tube_framenum_all<iframe]
+
+                bhv_events_plot = np.hstack([look_at_other_framenum_plot,look_at_lever_framenum_plot,look_at_tube_framenum_plot])
+                nplotframes = np.shape(bhv_events_plot)[0]
+
+                for iplotframe in np.arange(0,nplotframes,1):
+                    bhv_events_iframe = bhv_events_plot[iplotframe]
+                    if (ianimal == 0):
+                        if (np.isin(bhv_events_iframe,look_at_other_framenum_plot)): 
+                            ax2.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = colors[np.absolute(ianimal-1)])
+                        elif (np.isin(bhv_events_iframe,look_at_lever_framenum_plot)): 
+                            ax2.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = 'g')
+                        elif (np.isin(bhv_events_iframe,look_at_tube_framenum_plot)): 
+                            ax2.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = 'y')
+                        else:
+                            ax2.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = '0.5')
+                    elif (ianimal == 1):
+                        if (np.isin(bhv_events_iframe,look_at_other_framenum_plot)): 
+                            ax3.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = colors[np.absolute(ianimal-1)])
+                        elif (np.isin(bhv_events_iframe,look_at_lever_framenum_plot)): 
+                            ax3.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = 'g')
+                        elif (np.isin(bhv_events_iframe,look_at_tube_framenum_plot)): 
+                            ax3.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = 'y')
+                        else:
+                            ax3.plot([bhv_events_iframe,bhv_events_iframe],[0,1],'-',color = '0.5')                          
+                    
 
             writer.grab_frame()            
