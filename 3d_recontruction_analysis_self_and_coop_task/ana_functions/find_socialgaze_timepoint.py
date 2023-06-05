653400cdb038d00ec1fe6f8946d06484dd3314de
wilburshi <lajumboshiwk@gmail.com>
2023-06-05 10:19:01 -0400

    rebuild the code for 3d tracking analysis

--- 3d_recontruction_analysis_self_and_coop_task/ana_functions/find_socialgaze_timepoint.py
+++ 3d_recontruction_analysis_self_and_coop_task/ana_functions/find_socialgaze_timepoint.py
@@ -1,5 +1,5 @@
 # function - find social gaze time point
-def find_socialgaze_timepoint(body_part_locs_camera23, body_part_locs_camera12, body_part_locs_merge, angle_thres):
+def find_socialgaze_timepoint(body_part_locs_merge, min_length, angle_thres):
     
     import pandas as pd
     import numpy as np
@@ -9,110 +9,156 @@ def find_socialgaze_timepoint(body_part_locs_camera23, body_part_locs_camera12, 
     import warnings
     import pickle
 
-    animal_names_unique = pd.unique(pd.DataFrame(body_part_locs_camera23.keys()).iloc[:,0])
-    body_parts_unique = pd.unique(pd.DataFrame(body_part_locs_camera23.keys()).iloc[:,1])
-    min_length = np.min([list(body_part_locs_camera23.values())[0].shape[0],list(body_part_locs_camera12.values())[0].shape[0]])
+    animal_names_unique = pd.unique(pd.DataFrame(body_part_locs_merge.keys()).iloc[:,0])
+    nanimals = animal_names_unique.shape[0]    
+    body_parts_unique = pd.unique(pd.DataFrame(body_part_locs_merge.keys()).iloc[:,1])
+    nbodies = body_parts_unique.shape[0]
+
 
     warnings.filterwarnings('ignore')
     eye_direction_merge = {}
     eye_contact_or_not_merge = {}
     look_at_face_or_not_merge = {}
+
     
-    for iname in animal_names_unique:
+    for ianimal in np.arange(0,nanimals,1):
+
+        iname = animal_names_unique[ianimal]
+        # 
+        if (iname == animal_names_unique[0]): 
+            iname_other = animal_names_unique[1]
+        elif (iname == animal_names_unique[1]): 
+            iname_other = animal_names_unique[0]
+
         eye_dir_frames = []
         eye_contact_frames = []
         look_at_face_frames = []
         
         for iframe in np.arange(0,min_length,1):
+
+            # define the position of the animal's face part
+            #
+            # fill in the nan gaps
+            if (iframe > 0):
+                meaneye_loc_old = meaneye_loc
+                meantuft_loc_old = meantuft_loc
+                mass_loc_old = mass_loc
+                whiblz_loc_old = whiblz_loc
+            # 
             lefteye_loc = np.array(body_part_locs_merge[(iname,'leftEye')])[iframe,:]
             righteye_loc = np.array(body_part_locs_merge[(iname,'rightEye')])[iframe,:]
             lefttuft_loc = np.array(body_part_locs_merge[(iname,'leftTuft')])[iframe,:]
             righttuft_loc = np.array(body_part_locs_merge[(iname,'rightTuft')])[iframe,:]
             whiblz_loc = np.array(body_part_locs_merge[(iname,'whiteBlaze')])[iframe,:]
             mouth_loc = np.array(body_part_locs_merge[(iname,'mouth')])[iframe,:]
-        
-            Vect1 = lefteye_loc - righteye_loc
-            Vect2 = whiblz_loc - mouth_loc
-            Vect3 = lefttuft_loc - lefteye_loc
-            Vect4 = righttuft_loc - righteye_loc
-        
-            try:       
-                Vect1 = Vect1 / scipy.linalg.norm(Vect1)
-                Vect2 = Vect2 / scipy.linalg.norm(Vect2) 
-            except:
-                Vect1 = Vect1
-                Vect2 = Vect2
-            eyesight_dir = np.cross(Vect1, Vect2)
-        
-            if ((np.dot(eyesight_dir, Vect3)>0) | (np.dot(eyesight_dir, Vect4)>0)):
-                eyesight_dir = -eyesight_dir
-        
-            eye_dir_frames.append(eyesight_dir)      
-        
+            #
+            meaneye_loc = np.nanmean(np.vstack((lefteye_loc,righteye_loc)),axis=0)
+            #
+            if ((np.sum(np.isnan(meaneye_loc))>0)&(iframe>0)):
+                meaneye_loc = meaneye_loc_old
+            # 
+            meantuft_loc = np.nanmean(np.vstack((lefttuft_loc,righttuft_loc)),axis=0)
+            if ((np.sum(np.isnan(meantuft_loc))>0)&(iframe>0)):
+                meantuft_loc = meantuft_loc_old
+            #  
+            mass_loc = np.nanmean(np.vstack((lefteye_loc,righteye_loc,lefteye_loc,righteye_loc,whiblz_loc,mouth_loc)),axis=0)
+            if ((np.sum(np.isnan(mass_loc))>0)&(iframe>0)):
+                mass_loc = mass_loc_old   
+            # 
+            if ((np.sum(np.isnan(whiblz_loc))>0)&(iframe>0)):
+                whiblz_loc = whiblz_loc_old         
+
             # examine whether this animal is looking at the other's eyes or face
-            if (iname == animal_names_unique[0]): 
-                iname_other = animal_names_unique[1]
-            elif (iname == animal_names_unique[1]): 
-                iname_other = animal_names_unique[0]
-            
+            #
+            # fill in the nan gaps
+            if (iframe > 0):
+                meaneye_loc_other_old = meaneye_loc_other
+                meantuft_loc_other_old = meantuft_loc_other
+                mass_loc_other_old = mass_loc_other
+                whiblz_loc_other_old = whiblz_loc_other
+            # 
             lefteye_loc_other = np.array(body_part_locs_merge[(iname_other,'leftEye')])[iframe,:]
             righteye_loc_other = np.array(body_part_locs_merge[(iname_other,'rightEye')])[iframe,:]
             lefttuft_loc_other = np.array(body_part_locs_merge[(iname_other,'leftTuft')])[iframe,:]
             righttuft_loc_other = np.array(body_part_locs_merge[(iname_other,'rightTuft')])[iframe,:]
             whiblz_loc_other = np.array(body_part_locs_merge[(iname_other,'whiteBlaze')])[iframe,:]
             mouth_loc_other = np.array(body_part_locs_merge[(iname_other,'mouth')])[iframe,:]
+            #
+            meaneye_loc_other = np.nanmean(np.vstack((lefteye_loc_other,righteye_loc_other)),axis=0)
+            if ((np.sum(np.isnan(meaneye_loc_other))>0)&(iframe>0)):
+                meaneye_loc_other = meaneye_loc_other_old
+            # 
+            meantuft_loc_other = np.nanmean(np.vstack((lefttuft_loc_other,righttuft_loc_other)),axis=0)
+            if ((np.sum(np.isnan(meantuft_loc_other))>0)&(iframe>0)):
+                meantuft_loc_other = meantuft_loc_other_old
+            #  
+            mass_loc_other = np.nanmean(np.vstack((lefteye_loc_other,righteye_loc_other,lefteye_loc_other,righteye_loc_other,whiblz_loc_other,mouth_loc_other)),axis=0)
+            if ((np.sum(np.isnan(mass_loc_other))>0)&(iframe>0)):
+                mass_loc_other = mass_loc_other_old 
+            # 
+            if ((np.sum(np.isnan(whiblz_loc_other))>0)&(iframe>0)):
+                whiblz_loc_other = whiblz_loc_other_old      
+
+
+            # define important vectors for defining head gaze direction
+            # fill in the nan gaps
+            if (iframe > 0):
+                eyesight_dir_old = eyesight_dir
+            #
+            Vect1 = lefteye_loc - righteye_loc
+            Vect2 = whiblz_loc - mouth_loc
+            Vect3 = lefttuft_loc - lefteye_loc
+            Vect4 = righttuft_loc - righteye_loc
+            Vect5 = meantuft_loc - meaneye_loc
+            try:       
+                Vect1 = Vect1 / scipy.linalg.norm(Vect1)
+                Vect2 = Vect2 / scipy.linalg.norm(Vect2) 
+                Vect3 = Vect3 / scipy.linalg.norm(Vect3)
+                Vect4 = Vect4 / scipy.linalg.norm(Vect4)
+                Vect5 = Vect5 / scipy.linalg.norm(Vect5)
+            except:
+                Vect1 = Vect1 / np.nanmax(np.absolute(Vect1))
+                Vect2 = Vect2 / np.nanmax(np.absolute(Vect2))
+                Vect3 = Vect3 / np.nanmax(np.absolute(Vect3))
+                Vect4 = Vect4 / np.nanmax(np.absolute(Vect4))
+                Vect5 = Vect5 / np.nanmax(np.absolute(Vect5))
+            eyesight_dir = np.cross(Vect1, Vect2)
+            try:
+                eyesight_dir = eyesight_dir / scipy.linalg.norm(eyesight_dir)
+            except:
+                eyesight_dir = eyesight_dir / np.nanmax(np.absolute(eyesight_dir))
+            #
+            if (np.dot(eyesight_dir, Vect5)>0):
+                eyesight_dir = -eyesight_dir
+            #
+            if ((np.sum(np.isnan(eyesight_dir))>0)&(iframe>0)):
+                eyesight_dir = eyesight_dir_old
+            eye_dir_frames.append(eyesight_dir)      
+ 
         
             # where left eye is looking
             # vector between body part
-            vect1_lefteye = lefteye_loc_other - lefteye_loc
-            vect2_lefteye = righteye_loc_other - lefteye_loc
-            vect3_lefteye = lefttuft_loc_other - lefteye_loc
-            vect4_lefteye = righttuft_loc_other - lefteye_loc
-            vect5_lefteye = whiblz_loc_other - lefteye_loc
-            vect6_lefteye = mouth_loc_other - lefteye_loc
-            # angle between body part vector and eyesight direction
-            # angle1_lefteye =  np.sign(np.dot(eyesight_dir,vect1_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect1_lefteye/np.linalg.norm(vect1_lefteye)), -1.0, 1.0))       
-            # angle2_lefteye =  np.sign(np.dot(eyesight_dir,vect2_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect2_lefteye/np.linalg.norm(vect2_lefteye)), -1.0, 1.0))
-            # angle3_lefteye =  np.sign(np.dot(eyesight_dir,vect3_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect3_lefteye/np.linalg.norm(vect3_lefteye)), -1.0, 1.0))
-            # angle4_lefteye =  np.sign(np.dot(eyesight_dir,vect4_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect4_lefteye/np.linalg.norm(vect4_lefteye)), -1.0, 1.0))
-            # angle5_lefteye =  np.sign(np.dot(eyesight_dir,vect5_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect5_lefteye/np.linalg.norm(vect5_lefteye)), -1.0, 1.0))
-            # angle6_lefteye =  np.sign(np.dot(eyesight_dir,vect6_lefteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect6_lefteye/np.linalg.norm(vect6_lefteye)), -1.0, 1.0))
-            angle1_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect1_lefteye/np.linalg.norm(vect1_lefteye)), -1.0, 1.0))       
-            angle2_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect2_lefteye/np.linalg.norm(vect2_lefteye)), -1.0, 1.0))
-            angle3_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect3_lefteye/np.linalg.norm(vect3_lefteye)), -1.0, 1.0))
-            angle4_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect4_lefteye/np.linalg.norm(vect4_lefteye)), -1.0, 1.0))
-            angle5_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect5_lefteye/np.linalg.norm(vect5_lefteye)), -1.0, 1.0))
-            angle6_lefteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect6_lefteye/np.linalg.norm(vect6_lefteye)), -1.0, 1.0))
+            vect_face_eye = mass_loc_other - meaneye_loc
+            vect_eye_eye = meaneye_loc_other - meaneye_loc
+            try:
+                vect_face_eye = vect_face_eye / scipy.linalg.norm(vect_face_eye)
+            except:
+                vect_face_eye = vect_face_eye / np.nanmax(np.absolute(vect_face_eye))
+            try:
+                vect_eye_eye = vect_eye_eye / scipy.linalg.norm(vect_eye_eye)
+            except:
+                vect_eye_eye = vect_eye_eye / np.nanmax(np.absolute(vect_eye_eye))
+
+
+            angle_face_eye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect_face_eye/np.linalg.norm(vect_face_eye)), -1.0, 1.0))       
+            angle_eye_eye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect_eye_eye/np.linalg.norm(vect_eye_eye)), -1.0, 1.0))
+            
         
-            # where right eye is looking
-            # vector between body part
-            vect1_righteye = lefteye_loc_other - righteye_loc
-            vect2_righteye = righteye_loc_other - righteye_loc
-            vect3_righteye = lefttuft_loc_other - righteye_loc
-            vect4_righteye = righttuft_loc_other - righteye_loc
-            vect5_righteye = whiblz_loc_other - righteye_loc
-            vect6_righteye = mouth_loc_other - righteye_loc
-            # angle between body part vector and eyesight direction
-            # angle1_righteye =  np.sign(np.dot(eyesight_dir,vect1_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect1_righteye/np.linalg.norm(vect1_righteye)), -1.0,1.0))       
-            # angle2_righteye =  np.sign(np.dot(eyesight_dir,vect2_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect2_righteye/np.linalg.norm(vect2_righteye)), -1.0, 1.0))
-            # angle3_righteye =  np.sign(np.dot(eyesight_dir,vect3_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect3_righteye/np.linalg.norm(vect3_righteye)), -1.0, 1.0))
-            # angle4_righteye =  np.sign(np.dot(eyesight_dir,vect4_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect4_righteye/np.linalg.norm(vect4_righteye)), -1.0, 1.0))
-            # angle5_righteye =  np.sign(np.dot(eyesight_dir,vect5_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect5_righteye/np.linalg.norm(vect5_righteye)), -1.0, 1.0))
-            # angle6_righteye =  np.sign(np.dot(eyesight_dir,vect6_righteye))*np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect6_righteye/np.linalg.norm(vect6_righteye)), -1.0, 1.0))
-            angle1_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect1_righteye/np.linalg.norm(vect1_righteye)), -1.0, 1.0))       
-            angle2_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect2_righteye/np.linalg.norm(vect2_righteye)), -1.0, 1.0))
-            angle3_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect3_righteye/np.linalg.norm(vect3_righteye)), -1.0, 1.0))
-            angle4_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect4_righteye/np.linalg.norm(vect4_righteye)), -1.0, 1.0))
-            angle5_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect5_righteye/np.linalg.norm(vect5_righteye)), -1.0, 1.0))
-            angle6_righteye =  np.arccos(np.clip(np.dot(eyesight_dir/np.linalg.norm(eyesight_dir), vect6_righteye/np.linalg.norm(vect6_righteye)), -1.0, 1.0))
-        
-            lefteye_contact_thres = ((angle1_lefteye>0)&(angle1_lefteye<angle_thres))|((angle2_lefteye>0)&(angle2_lefteye<angle_thres))
-            lefteye_lookface_thres = ((angle3_lefteye>0)&(angle3_lefteye<angle_thres))|((angle4_lefteye>0)&(angle4_lefteye<angle_thres))|((angle5_lefteye>0)&(angle5_lefteye<angle_thres))|((angle6_lefteye>0)&(angle6_lefteye<angle_thres))
-            righteye_contact_thres = ((angle1_righteye>0)&(angle1_righteye<angle_thres))|((angle2_righteye>0)&(angle2_righteye<angle_thres))
-            righteye_lookface_thres = ((angle3_righteye>0)&(angle3_righteye<angle_thres))|((angle4_righteye>0)&(angle4_righteye<angle_thres))|((angle5_righteye>0)&(angle5_righteye<angle_thres))|((angle6_righteye>0)&(angle6_righteye<angle_thres))
-        
-            eye_contact_frames.append(np.int(lefteye_contact_thres|righteye_contact_thres))
-            look_at_face_frames.append(np.int(lefteye_contact_thres|righteye_contact_thres|lefteye_lookface_thres|righteye_lookface_thres))
+            eye_contact_thres = ((angle_eye_eye>0)&(angle_eye_eye<angle_thres))
+            eye_lookface_thres = ((angle_face_eye>0)&(angle_face_eye<angle_thres))
+                  
+            eye_contact_frames.append(np.int(eye_contact_thres))
+            look_at_face_frames.append(np.int(eye_lookface_thres))
         
         # save to the summarized data
         eye_direction_merge[(iname)] = eye_dir_frames
